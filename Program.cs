eda6ab17005a25a2142ecf405f7268836d94d65a
hayzedzee <azeezahayomide@gmail.com>
2022-04-12 06:23:29 -0700

    variable and datatypes

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab2work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intvariable = 56;
+            string strvariable = "WellaHealth";
+            char charvariable = 'M';
+            float floatvariable = 56.2F;
+            double doublevariable = 123.56;
+            bool boolvariable = true;
+            short shortvariable = 19;
+            decimal decimalvariable = 5.110e-21M;
+            long longvariable = -9879;
+            byte bytevariable = 212;
+
+
+            Console.WriteLine(intvariable);
+            Console.WriteLine(strvariable);
+            Console.WriteLine(charvariable);
+            Console.WriteLine(floatvariable);
+            Console.WriteLine(doublevariable);
+            Console.WriteLine(boolvariable);
+            Console.WriteLine(shortvariable);
+            Console.WriteLine(decimalvariable);
+            Console.WriteLine(longvariable);
+            Console.WriteLine(bytevariable);
+        }
+    }
+}
